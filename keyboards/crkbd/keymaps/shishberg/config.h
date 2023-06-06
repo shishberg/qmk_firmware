ae10242d6df93ac11e1db8bf506fc2fc8029f618
David McLeish <dave@mcleish.id.au>
2023-06-06 12:14:51 +1000

    keyboards/crkbd/keymaps/shishberg

---
+++ keyboards/crkbd/keymaps/shishberg/config.h
@@ -0,0 +1,91 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+//#define USE_SERIAL_PD2
+//#undef USE_I2C
+#define SELECT_SOFT_SERIAL_SPEED 1
+
+#define TAPPING_TERM 300
+#define TAPPING_TOGGLE 2
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
+
+#ifdef RGBLIGHT_ENABLE
+    #undef RGBLED_NUM
+    #define RGBLIGHT_ANIMATIONS
+    #define RGBLED_NUM 27
+    #define RGBLIGHT_LIMIT_VAL 120
+    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_SAT_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
+#endif
+
+#ifdef RGB_MATRIX_ENABLE
+    #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+    #define RGB_MATRIX_KEYPRESSES
+#    define DISABLE_RGB_MATRIX_ALPHAS_MODS
+#    define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#    define DISABLE_RGB_MATRIX_BREATHING
+#    define DISABLE_RGB_MATRIX_BAND_SAT
+#    define DISABLE_RGB_MATRIX_BAND_VAL
+#    define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#    define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+#    define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#    define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+// #define DISABLE_RGB_MATRIX_CYCLE_ALL
+#    define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#    define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#    define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
+#    define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+// #define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#    define DISABLE_RGB_MATRIX_DUAL_BEACON
+#    define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
+#    define DISABLE_RGB_MATRIX_CYCLE_SPIRAL
+#    define DISABLE_RGB_MATRIX_RAINBOW_BEACON
+#    define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+#    define DISABLE_RGB_MATRIX_RAINDROPS
+#    define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+// #    define DISABLE_RGB_MATRIX_TYPING_HEATMAP
+// #    define DISABLE_RGB_MATRIX_DIGITAL_RAIN
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+// // #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+// #    define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+// // #define DISABLE_RGB_MATRIX_SPLASH
+// #    define DISABLE_RGB_MATRIX_MULTISPLASH
+// #    define DISABLE_RGB_MATRIX_SOLID_SPLASH
+// #    define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#endif
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
